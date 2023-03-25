8dcfc39d14310779f941ca2ab536bf0c31b04d89
gratka <socer404@gmail.com>
2023-03-25 14:48:09 +0100

    Added canvas

--- src/main/java/com/example/graphproject/functions/DijkstraCanvasPrinter.java
+++ src/main/java/com/example/graphproject/functions/DijkstraCanvasPrinter.java
@@ -1,4 +1,88 @@
 package com.example.graphproject.functions;
 
-public class DijkstraCanvasPrinter {
+
+import com.example.graphproject.graphUtils.CanvasGraphCoordinatesDeteminer;
+import com.example.graphproject.graphUtils.GraphCanvasPrinter;
+import com.example.graphproject.graphUtils.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class DijkstraCanvasPrinter extends DijkstraSolver {
+    private final GraphCanvasPrinter graphCanvasPrinter;
+
+    public DijkstraCanvasPrinter(Node source, GraphCanvasPrinter graphCanvasPrinter) {
+        this.graphCanvasPrinter = graphCanvasPrinter;
+        this.sourceNode = source;
+        this.graph = graphCanvasPrinter.getGraph();
+        solve();
+    }
+
+    @Override
+    public void print(Node destinationNode) {
+        if (distances[destinationNode.getNodeId()].getOtherNodeId() == -1) {
+            return;
+        }
+        Node currentNode = destinationNode;
+        Node nextNode;
+
+        Canvas graphCanvas = graphCanvasPrinter.getGraphCanvas();
+        CanvasGraphCoordinatesDeteminer graphCoordinatesDeteminer = graphCanvasPrinter.getGraphCoordinatesDeteminer();
+
+        double pathSize = graphCoordinatesDeteminer.getNodeSize() / 2;
+        GraphicsContext graphicsContext = graphCanvas.getGraphicsContext2D();
+        graphicsContext.setFill(Color.PURPLE);
+
+        graphicsContext.fillOval(graphCoordinatesDeteminer.getCenterOfNodeX(currentNode) - (pathSize / 2),
+                graphCoordinatesDeteminer.getCenterOfNodeY(currentNode) - (pathSize / 2), pathSize, pathSize);
+        while (currentNode != sourceNode) {
+            nextNode = graph.getNodes().get(distances[currentNode.getNodeId()].getOtherNodeId());
+
+            if (nextNode.getNodeId() == currentNode.getNodeId() + 1) {
+                graphicsContext.fillRect(graphCoordinatesDeteminer.getCenterOfNodeX(currentNode),
+                        graphCoordinatesDeteminer.getCenterOfNodeY(currentNode) - (pathSize / 2),
+                        graphCoordinatesDeteminer.getNodeSize(), pathSize);
+            }
+            if (nextNode.getNodeId() == currentNode.getNodeId() - 1) {
+                graphicsContext.fillRect(graphCoordinatesDeteminer.getCenterOfNodeX(nextNode),
+                        graphCoordinatesDeteminer.getCenterOfNodeY(nextNode) - (pathSize / 2),
+                        graphCoordinatesDeteminer.getNodeSize(), pathSize);
+            }
+            if (nextNode.getNodeId() == currentNode.getNodeId() + graph.getCollumns()) {
+                graphicsContext.fillRect(graphCoordinatesDeteminer.getCenterOfNodeX(currentNode) - (pathSize / 2),
+                        graphCoordinatesDeteminer.getCenterOfNodeY(currentNode),
+                        pathSize, graphCoordinatesDeteminer.getNodeSize());
+            }
+            if (nextNode.getNodeId() == currentNode.getNodeId() - graph.getCollumns()) {
+                graphicsContext.fillRect(graphCoordinatesDeteminer.getCenterOfNodeX(nextNode) - (pathSize / 2),
+                        graphCoordinatesDeteminer.getCenterOfNodeY(nextNode),
+                        pathSize, graphCoordinatesDeteminer.getNodeSize());
+            }
+
+            graphicsContext.fillOval(graphCoordinatesDeteminer.getCenterOfNodeX(nextNode) - (pathSize / 2),
+                    graphCoordinatesDeteminer.getCenterOfNodeY(nextNode) - (pathSize / 2), pathSize, pathSize);
+            currentNode = graph.getNodes().get(distances[currentNode.getNodeId()].getOtherNodeId());
+        }
+    }
+
+    public void makeGradient() {
+        double maxValue = findMaxDistance();
+
+        for (NodeDistance nodeDistance : distances) {
+            graphCanvasPrinter.paintNode(graph.getNodes().get(nodeDistance.getNodeId()),
+                    nodeDistance.getDistance(), 0, maxValue);
+        }
+    }
+
+    public double findMaxDistance() {
+        double maxValue = 0;
+        for (NodeDistance nodeDistance : distances) {
+            if (nodeDistance.getDistance() > maxValue) {
+                maxValue = nodeDistance.getDistance();
+            }
+        }
+        return maxValue;
+    }
+
+
 }
--- src/main/java/com/example/graphproject/graphUtils/GraphCanvasPrinter.java
+++ src/main/java/com/example/graphproject/graphUtils/GraphCanvasPrinter.java
@@ -1,4 +1,118 @@
 package com.example.graphproject.graphUtils;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class GraphCanvasPrinter {
+    private final Graph graph;
+
+    private final Canvas graphCanvas;
+
+    private final CanvasGraphCoordinatesDeteminer graphCoordinatesDeteminer;
+
+    private final ColorPicker colorPicker;
+
+    public GraphCanvasPrinter(Graph graph, Canvas graphCanvas) {
+        this.graph = graph;
+        this.graphCanvas = graphCanvas;
+
+        graphCoordinatesDeteminer = new CanvasGraphCoordinatesDeteminer(graph, graphCanvas);
+        colorPicker = new ColorPicker();
+    }
+
+    public void print() {
+        GraphicsContext graphicsContext = graphCanvas.getGraphicsContext2D();
+        graphicsContext.setFill(Color.WHITE);
+
+        int nodeIndex = 0;
+
+        double nodeSize = graphCoordinatesDeteminer.getNodeSize();
+        double ovalSize = graphCoordinatesDeteminer.getOvalSize();
+        double edgeSize = graphCoordinatesDeteminer.getEdgeSize();
+
+        double[] rangeOfEdges = graph.determineEdgeRange();
+
+        double nodeX;
+        double nodeY = 10;
+
+
+        double nodeCenterX;
+        double nodeCenterY = 10 + (ovalSize / 2);
+
+        for (int row = 0; row < graph.getRows(); row++) {
+            nodeX = 10;
+            nodeCenterX = 10 + (ovalSize / 2);
+            for (int collumn = 0; collumn < graph.getCollumns(); collumn++) {
+                for (Edge edge : graph.getNodes().get(nodeIndex).getEdgesOfNode()) {
+                    if (edge.getNodeId() == nodeIndex - 1 && graph.getCollumns() != 1) {
+                        graphicsContext.setFill(colorPicker.determineColorForCanvas(edge.getWeightOfEdge(),
+                                rangeOfEdges[0], rangeOfEdges[1]));
+                        graphicsContext.fillRect(nodeCenterX - nodeSize, nodeCenterY - (edgeSize / 2), nodeSize, edgeSize);
+
+                        graphicsContext.setFill(Color.WHITE);
+                        graphicsContext.fillOval(nodeX - nodeSize, nodeY, ovalSize, ovalSize);
+                    }
+                    if (edge.getNodeId() == nodeIndex + 1 && graph.getCollumns() != 1) {
+                        graphicsContext.setFill(colorPicker.determineColorForCanvas(edge.getWeightOfEdge(),
+                                rangeOfEdges[0], rangeOfEdges[1]));
+                        graphicsContext.fillRect(nodeCenterX, nodeCenterY - (edgeSize / 2), nodeSize, edgeSize);
+                    }
+                    if (edge.getNodeId() == nodeIndex + graph.getCollumns() && graph.getRows() != 1) {
+                        graphicsContext.setFill(colorPicker.determineColorForCanvas(edge.getWeightOfEdge(),
+                                rangeOfEdges[0], rangeOfEdges[1]));
+                        graphicsContext.fillRect(nodeCenterX - (edgeSize / 2), nodeCenterY, edgeSize, nodeSize);
+                    }
+                    if (edge.getNodeId() == nodeIndex - graph.getCollumns() && graph.getRows() != 1) {
+                        graphicsContext.setFill(colorPicker.determineColorForCanvas(edge.getWeightOfEdge(),
+                                rangeOfEdges[0], rangeOfEdges[1]));
+                        graphicsContext.fillRect(nodeCenterX - (edgeSize / 2), nodeCenterY - nodeSize, edgeSize, nodeSize);
+
+                        graphicsContext.setFill(Color.WHITE);
+                        graphicsContext.fillOval(nodeX, nodeY - nodeSize, ovalSize, ovalSize);
+                    }
+                }
+                graphicsContext.setFill(Color.WHITE);
+                graphicsContext.fillOval(nodeX, nodeY, ovalSize, ovalSize);
+                nodeIndex++;
+                nodeX += nodeSize;
+                nodeCenterX += nodeSize;
+            }
+            nodeY += nodeSize;
+            nodeCenterY += nodeSize;
+
+        }
+
+    }
+
+    public Node selectNode(double x, double y) {
+        return graphCoordinatesDeteminer.findNode(x, y);
+    }
+
+
+    public void paintNode(Node node, Color color) {
+        GraphicsContext graphicsContext = graphCanvas.getGraphicsContext2D();
+        graphicsContext.setFill(color);
+        graphicsContext.fillOval(graphCoordinatesDeteminer.getNodeX(node), graphCoordinatesDeteminer.getNodeY(node),
+                graphCoordinatesDeteminer.getOvalSize(), graphCoordinatesDeteminer.getOvalSize());
+    }
+
+    public void paintNode(Node node, double value, double startOfRange, double endOfRange) {
+        GraphicsContext graphicsContext = graphCanvas.getGraphicsContext2D();
+        graphicsContext.setFill(colorPicker.determineColorForCanvas(value, startOfRange, endOfRange));
+        graphicsContext.fillOval(graphCoordinatesDeteminer.getNodeX(node), graphCoordinatesDeteminer.getNodeY(node),
+                graphCoordinatesDeteminer.getOvalSize(), graphCoordinatesDeteminer.getOvalSize());
+    }
+
+    public Canvas getGraphCanvas() {
+        return graphCanvas;
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
+
+    public CanvasGraphCoordinatesDeteminer getGraphCoordinatesDeteminer() {
+        return graphCoordinatesDeteminer;
+    }
 }
